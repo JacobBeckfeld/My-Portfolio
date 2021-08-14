5eb735981cf816bce79cb0b7047d23e898d8d42c
JacobBeckfeld <jacbeck456@gmail.com>
2021-08-13 23:56:40 -0500

    idk

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Portfolio from "./components/Portfolio2"
+import Portfolio from "./components/Portfolio"
 import "./App.css"
 
-const App = () => <Portfolio2/>;
+const App = () => <Portfolio/>;
 
 export default App;
