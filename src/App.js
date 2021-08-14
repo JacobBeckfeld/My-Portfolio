b96f179cd918390110e4cc2d07ce9c805edc25a2
JacobBeckfeld <jacbeck456@gmail.com>
2021-08-13 20:54:25 -0500

    trying to fix heroku problems

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Portfolio from "./components/Portfolio";
+import Portfolio from "./components/Portfolio.js";
 import "./App.css"
 
 const App = () => <Portfolio/>;
