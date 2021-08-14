a5686baed900892e8e6ba3c5310564f974c8b18d
JacobBeckfeld <jacbeck456@gmail.com>
2021-08-14 14:16:41 -0500

    added github and linkdin

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -14,6 +14,14 @@ const About = () => {
                             <p className="card-text">My name is Jacob Beckfeld and I am a full-stack developer from minneapolis MN. </p>
                             <p className="card-text">I work with HTML/CSS, JavaScript, NodeJS, Express, MySQL, MongoDB and React.</p>
                             <p className="card-text">I have a passion for video games and hope to one day be working on developing them.</p>
+                            <div className="row">
+                                <a href="https://www.linkedin.com/in/jacob-beckfeld-2a6088213/" className="col-4">
+                                    <img src="linkdin-icon.png" className="img-fluid"></img>
+                                </a>
+                                <a href="https://github.com/JacobBeckfeld" className="col-6">
+                                    <img src="github-icon.png" className="img-fluid"></img>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
